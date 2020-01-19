048f7676bc74c1ca3a84b89aa50e31c95ac3fe24
Janice-M <janiceink001@gmail.com>
2020-01-19 23:47:35 +0300

    createBlock function

--- js/blocks.js
+++ js/blocks.js
@@ -1,1 +1,5 @@
-function createBlock(name, value, contents)
+function createBlock(name, value, contents){
+    var item = elem('div',
+        {'class': 'block', draggable: true, 'data-name': name},
+        [name]
+    );
